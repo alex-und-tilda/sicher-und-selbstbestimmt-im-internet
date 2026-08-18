e704b7604b0cdeeb01118e2446060ecaac980155
Frank Runge <runge.stifttilbeck@icloud.com>
2026-08-18 20:25:34 +0200

    jo

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    Version: update CACHE_VERSION bei jeder Veröffentlichung
    ============================================================= */
 
-const CACHE_VERSION = "v2026-11b";
+const CACHE_VERSION = "v2026-11d";
 const CACHE_NAME    = "sicher-im-netz-" + CACHE_VERSION;
 /* Altlast: früher lagen die Piktogramme bei static.arasaac.org.
    Heute sind es eigene SVGs in assets/pictograms/. Dieser alte Cache
